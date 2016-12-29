410cf89180b6d7e4366dc54e8bcb3284d0a702a7
byoungki.jeon <byoungki.jeon@gmail.com>
2016-12-29 16:58:30 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	bonjour.py

--- bonjour.py
+++ bonjour.py
@@ -1,4 +1,21 @@
 # coding=utf-8
+=======
+
+=======
+ 
+>>>>>>> origin/master
+=======
+ 
+>>>>>>> origin/master
+"""
+##########
+lecture 1
+##########
+"""
+
+>>>>>>> origin/master
+=======
+>>>>>>> origin/master
 print("안녕하세요")
 # 주석 다는 것
 """
@@ -17,17 +34,21 @@ comments 2
 """
 
 """
-###### lecture 2
+##########
+lecture 2
+##########
 """
 _2Myname = '에이미'
 print(_2Myname)
 
-print("je m'allell" + _2Myname)
-print('je m"allell' + _2Myname)
+print("je m'allell " + _2Myname)
+print('je m"allell ' + _2Myname)
 
-name = _2Myname * 2
+name = _2Myname * 3
 print(name)
 
 """
-###### lecture 3
+##########
+lecture 3
+##########
 """
